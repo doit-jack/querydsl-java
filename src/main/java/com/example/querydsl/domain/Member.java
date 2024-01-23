0d16470f2d753921ea5a811a4be6f69ad2625465
github doit-jack <github jackyoon93@gmail.com>
2024-01-24 00:54:57 +0900

    24.01.23 - chapter3-ing

---
+++ src/main/java/com/example/querydsl/domain/Member.java
@@ -0,0 +1,57 @@
+package com.example.querydsl.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 막고 싶은데, JPA 스팩상 PROTECTED로 열어두어야 함
+@ToString(of = {"id", "username", "age"}) // ToString은 가급적 내부 필드만(연관관계 없는 필드)
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    public Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    /**
+     * 양방향 연관관계 한번에 처리 (연관관계 편의 메서드)
+     */
+    private void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
---
+++ src/test/java/com/example/querydsl/domain/MemberTest.java
@@ -0,0 +1,55 @@
+package com.example.querydsl.domain;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.mysema.commons.lang.Assert;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @BeforeEach
+    public void before() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        String qlString = "select m from Member m " + "where m.username = :username";
+        Member findMember = em.createQuery(qlString, Member.class)
+            .setParameter("username", "member1")
+            .getSingleResult();
+        assertThat(findMember.getUsername());
+    }
+
+
+}
